2c277739234f36766e21ff4cd6385650c4194ba0
terrafirma2021 <69117226+terrafirma2021@users.noreply.github.com>
2025-12-05 05:05:49 +0000

    up

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="border-t w-full">
-      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-16 text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+    <footer className="border-t w-full mb-0 pb-0">
+      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-16 text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4" style={{ paddingBottom: 0, marginBottom: 0 }}>
         <div className="flex items-center gap-3">
         
           <p className="text-center">
@@ -10,7 +10,6 @@ export function Footer() {
         </div>
 
       </div>
-      <div className="h-16"></div>
     </footer>
   );
 }
