7db905ccddac21dede3e7ba973fc6ab019b33549
terrafirma2021 <69117226+terrafirma2021@users.noreply.github.com>
2025-12-06 16:37:03 +0000

    up

--- app/[lang]/discord/page.tsx
+++ app/[lang]/discord/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation";
 
+// Force dynamic rendering since we're using redirect()
+export const dynamic = 'force-dynamic';
+
 export default function DiscordRedirect() {
   redirect("https://discord.com/invite/makcu");
 }
